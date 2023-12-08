a78047576dda58361b3b404ae6d8679c438c1a6a
Ragunanthan <66114285+ragunanthan@users.noreply.github.com>
2023-12-08 15:24:50 +0530

    Add files via upload

--- src/app/api/blogs/recent/route.ts
+++ src/app/api/blogs/recent/route.ts
@@ -1,14 +1,18 @@
 
+export const dynamic = "force-dynamic";
+
 import Blogs from "@/dbConnection";
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(req: any, {params} : any) {
   try {
+  
     const blogs = await Blogs.find()
     .sort({ createdAt: -1 }) // Sort in descending order based on timestamp
     .limit(4) ;
     
     return NextResponse.json({ data : blogs }, { status: 200 });
+   
   } catch (err) {
     console.log(err);
     return NextResponse.json({ message: "Error", err }, { status: 500 });
