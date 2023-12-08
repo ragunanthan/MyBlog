2b6a1386b016c3fc458da44b0ae12327bdf8ab10
Ragunanthan <66114285+ragunanthan@users.noreply.github.com>
2023-12-08 11:14:48 +0530

    Add files via upload

--- src/dbConnection.ts
+++ src/dbConnection.ts
@@ -1,6 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 
-mongoose.connect(`${process.env.MONGODB}`);
+mongoose.connect(`${process.env.MONGODB_URI}`);
 mongoose.Promise = global.Promise;
 
 const blogsSchema = new Schema(
